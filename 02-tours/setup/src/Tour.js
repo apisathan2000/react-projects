1beebbb38b3602ede7061ac80c8c68032d608c90
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2026-03-16 21:12:22 +0530

    Refactor code structure for improved readability and maintainability

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ tour, onNotInterested }) => {
+  const [isExpanded, setExpand] = useState(false);
+
+  const onExpandButtonClick = function () {
+    setExpand(!isExpanded);
+  };
+
+  return (
+    <article className="single-tour">
+      <img src={tour.image} className="single-tour-img" />
+
+      <footer>
+        <div className="tour-info">
+          <h4>{tour.name}</h4>
+          <h4 className="tour-price">$ {tour.price}</h4>
+        </div>
+
+        <p>
+          {isExpanded ? tour.info : `${tour.info.substring(0, 200)}...`}{" "}
+          <button onClick={onExpandButtonClick}>
+            {isExpanded ? `Show Less` : `Read More`}
+          </button>
+        </p>
+
+        <button onClick={() => onNotInterested(tour.id)} className="delete-btn">
+          Not Interested
+        </button>
+      </footer>
+    </article>
+  );
 };
+
+// ${info.substring(0, 200)}
 
 export default Tour;
