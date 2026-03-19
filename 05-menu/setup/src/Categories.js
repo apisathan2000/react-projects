9886d52690411050db4c4eedf4444215fa349851
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2026-03-19 20:51:49 +0530

    Refactor code structure for improved readability and maintainability

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ onFilterClick }) => {
+  return (
+    <div className="btn-container">
+      <button
+        className="filter-btn"
+        onClick={() => {
+          onFilterClick("all");
+        }}
+      >
+        All
+      </button>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          onFilterClick("breakfast");
+        }}
+      >
+        Breakfast
+      </button>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          onFilterClick("lunch");
+        }}
+      >
+        Lunch
+      </button>
+      <button
+        className="filter-btn"
+        onClick={() => {
+          onFilterClick("shakes");
+        }}
+      >
+        Shakes
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
