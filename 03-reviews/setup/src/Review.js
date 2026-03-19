416e892924b2b8f664eed05066c9c3a3ad88be01
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2026-03-19 06:56:07 +0530

    Enhance review component layout and functionality
    - Updated App.js to include a structured layout for reviews with a title and underline.
    - Implemented Review.js to manage and display individual reviews with navigation buttons.
    - Added functionality for previous, next, and random review selection.

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,56 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+
+  const previousPerson = function () {
+    setIndex((prevIndex) => {
+      return prevIndex === 0 ? people.length - 1 : prevIndex - 1;
+    });
+  };
+
+  const nextPerson = function () {
+    setIndex((prevIndex) => {
+      return prevIndex === people.length - 1 ? 0 : prevIndex + 1;
+    });
+  };
+
+  const randomPerson = function () {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = (index + 1) % people.length;
+    }
+    setIndex(randomNumber);
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={people[index].image} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+
+      <h4 className="author">{people[index].name}</h4>
+      <p className="job">{people[index].job}</p>
+      <p className="info">{people[index].text}</p>
+
+      <div className="">
+        <button className="prev-btn" onClick={previousPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
